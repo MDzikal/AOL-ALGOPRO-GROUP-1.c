56d259a0e3de71d4c104de8c527a2e57fe5bb641
repie <repie.sebastian@binus.ac.id>
2026-01-04 22:12:07 +0700

    Update Code AOL Algopro.c

--- Code AOL Algopro.c
+++ Code AOL Algopro.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void formatRupiah(int num, char *buffer) {
+    char temp[50];
+    int i, j = 0, len, pos = 0;
+
+    sprintf(temp, "%d", num);
+    len = strlen(temp);
+    char rev[50];
+
+    for (i = len - 1; i >= 0; i--) {
+        rev[pos++] = temp[i];
+        if ((len - i) % 3 == 0 && i != 0) {
+            rev[pos++] = '.';
+        }
+    }
+    rev[pos] = '\0';
+
+    len = strlen(rev);
+    for (i = len - 1; i >= 0; i--) {
+        buffer[j++] = rev[i];
+    }
+    buffer[j] = '\0';
+}
